894bc37a97222d6a3120cfa7d0c17191a9ca4348
SpecOpsRising <cadetfalls@gmail.com>
2025-02-23 20:39:53 -0500

    Project/ last mod 1 update.

--- app.py
+++ app.py
@@ -3,66 +3,61 @@ import requests                  # import the requests module to make HTTP reque
 import logging                   # import the logging module to log error messages
 app = Flask(__name__)       # create an instance of the Flask class/Create an app instance
 
-URL = "https://xckd.com/info.0.json"
-req = requests.get(url = URL)
-Data = req.json()
-
-@app.route('/')             # use the route() decorator to define a URL Route, in this case home '/'
+@app.route('/')             #Home route that allows you access to the home page and lets you link to the other pages of the website
 def home():
     try:
-        URL = "https://xckd.com/info.0.json"
-        response = requests.get(url = URL)
-        if (response.status_code != 200):
-            raise ValueError(f"Error connecting to XKCD please try again later")
-        Data = response.json()
-        return render_template('index.html', data=Data)
+        return render_template('index.html')
     except ValueError as e:
         logging.error(f"ValueError with Home Page : {e}")
         return render_template('errorPage.html', errorMessage = e)
 
-@app.route('/album')
-def album():
+@app.route('/breeds')
+def breeds():
     try:
-        URL = "https://xckd.com/info.0.json"
+        URL = "https://dogapi.dog/api/v2/breeds" #This project uses the Dog API to get information about dog breeds
         response = requests.get(url = URL)
         if (response.status_code != 200):
-            raise ValueError(f"Error connecting to XKCD please try again later")
-        Data = response.json()
-        return render_template('Album.html', data=Data)
+            raise ValueError(f"Error connecting to Dog API please try again later")
+        return render_template('breeds.html', data=response.json())
     except ValueError as e:
-        logging.error(f"ValueError with Album Page : {e}")
+        logging.error(f"ValueError with Breed List Page : {e}")
         return render_template('errorPage.html', errorMessage = e)
 
-@app.route('/selectComic/<comicNum>') # Second route with path parameter (part of the URL) called comicNum
-def selectComic(comicNum):
+@app.route('/selectedBreed/<id>') #Allows you to choose a breed from the breeds data given by the Dog API. Nav button defaults to the first breed.
+def selectBreed(id):
     try:
-        logging.debug(f"Select Comic URL with value : {comicNum}")  # log the value of comicNum
-        if not comicNum.isdigit():
-            raise ValueError(f"The requested comic number should be an integer. Received : {comicNum} instead")
-        int(comicNum)
-        if(int(comicNum) > 3000):
-            raise ValueError(f"No comic available for number {comicNum}. Please try a number less than 3001")
-        URL = "https://xckd.com/"+ comicNum +"/info.0.json"
+        logging.debug(f"Select Breed URL with value : {id}")
+        if (id == None or id == ""):
+            raise ValueError(f"The requested breed id is not valid, please choose from the available list. Inputed id: {id}")
+        URL = "https://dogapi.dog/api/v2/breeds/"+ id
         response = requests.get(url = URL)
-        print(response.status_code)
         if (response.status_code != 200):
-            raise ValueError(f"Error connecting to XKCD please try again later")
-        Data = response.json()
-        return render_template('selectComic.html', data=Data)
+            raise ValueError(f"Error connecting to Dog API please try again later")
+        return render_template('selectedBreed.html', data=response.json())
     except ValueError as e:
-        logging.error(f"ValueError with Comic Select : {e}")
+        logging.error(f"ValueError with Breed Select : {e}")
         return render_template('errorPage.html', errorMessage = e)
     except Exception as e:
-        logging.error(f"Exception with Comic Select : {e}")  # log the error message
+        logging.error(f"Exception with Breed Select : {e}")
         return render_template('errorPage.html', errorMessage = e)
-    # if(comicNum.isdigit() and int(comicNum) <= 3000):
-    #     URL = "https://xckd.com/"+ comicNum +"/info.0.json"
-    #     print(URL)
-    #     req = requests.get(url = URL)
-    #     Data = req.json()
-    #     return render_template('selectComic.html', data=Data)
-    # else:
-    #     return render_template('errorPage.html')
+    
+@app.route('/facts/<limit>')
+def facts(limit):
+    try:
+        logging.debug(f"Facts URL with value : {limit}")
+        if limit.isdigit() == False:
+            raise ValueError(f"The input is not a number please input a number. Recieved: {limit}")
+        URL = "https://dogapi.dog/api/v2/facts?limit=" + limit
+        response = requests.get(url = URL)
+        if (response.status_code != 200):
+            raise ValueError(f"Error connecting to Dog API please try again later")
+        return render_template('facts.html', data=response.json())
+    except ValueError as e:
+        logging.error(f"ValueError with Facts Page : {e}")
+        return render_template('errorPage.html', errorMessage = e)
+    except Exception as e:
+        logging.error(f"Exception with Facts Page : {e}")
+        return render_template('errorPage.html', errorMessage = e)
 
 
 if __name__ == '__main__':
